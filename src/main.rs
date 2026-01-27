c53551b36dd98d46c14b80e004ac4f46aee607c1
semanavasco <semanavasco@gmail.com>
2026-01-27 11:35:33 +0100

    feat: simple camera and player

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use bevy::{prelude::*, window::PrimaryWindow};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, spawn_camera)
+        .add_systems(Startup, spawn_player)
+        .run();
+}
+
+#[derive(Component)]
+pub struct Player;
+
+pub fn spawn_player(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.single().unwrap();
+
+    commands.spawn((
+        Sprite::from_image(asset_server.load("sprites/ball_blue_large.png")),
+        Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+        Player,
+    ));
+}
+
+pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
+    let window = window_query.single().unwrap();
+
+    commands.spawn((
+        Camera2d,
+        Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+    ));
 }
