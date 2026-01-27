f00c4d4e7348420e493e67aefa2848f1c4dcb834
semanavasco <semanavasco@gmail.com>
2026-01-27 12:05:01 +0100

    feat: add enemy spawn system

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use bevy::{prelude::*, window::PrimaryWindow};
+use rand::random;
 
 pub const PLAYER_SIZE: f32 = 64.0; // Player sprite size.
 pub const PLAYER_SPEED: f32 = 500.0;
+pub const NUMBER_OF_ENEMIES: usize = 4;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, spawn_camera)
         .add_systems(Startup, spawn_player)
+        .add_systems(Startup, spawn_enemies)
         .add_systems(Update, player_movement)
         .add_systems(Update, confine_player_movement)
         .run();
@@ -15,6 +18,9 @@ fn main() {
 
 #[derive(Component)]
 pub struct Player;
+
+#[derive(Component)]
+pub struct Enemy;
 
 pub fn spawn_player(
     mut commands: Commands,
@@ -37,6 +43,25 @@ pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<Pr
         Camera2d,
         Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
     ));
+}
+
+pub fn spawn_enemies(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.single().unwrap();
+
+    for _ in 0..NUMBER_OF_ENEMIES {
+        let random_x = random::<f32>() * window.width();
+        let random_y = random::<f32>() * window.height();
+
+        commands.spawn((
+            Sprite::from_image(asset_server.load("sprites/ball_red_large.png")),
+            Transform::from_xyz(random_x, random_y, 0.0),
+            Enemy,
+        ));
+    }
 }
 
 pub fn player_movement(
