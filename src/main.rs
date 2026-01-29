8e55b249fa4c724314299a4f34556ac826427e2e
semanavasco <semanavasco@gmail.com>
2026-01-29 22:41:38 +0100

    feat: add star spawn over time

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,13 @@ pub const NUMBER_OF_ENEMIES: usize = 4;
 pub const ENEMY_SPEED: f32 = 200.0;
 pub const NUMBER_OF_STARS: usize = 10;
 pub const STAR_SIZE: f32 = 30.0;
+pub const STAR_SPAWN_TIME: f32 = 1.0;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .init_resource::<Score>()
+        .init_resource::<StarSpawnTimer>()
         .add_systems(Startup, spawn_camera)
         .add_systems(Startup, spawn_player)
         .add_systems(Startup, spawn_enemies)
@@ -23,6 +25,8 @@ fn main() {
         .add_systems(Update, enemy_hit_player)
         .add_systems(Update, player_hit_star)
         .add_systems(Update, update_score)
+        .add_systems(Update, tick_star_spawn_timer)
+        .add_systems(Update, spawn_stars_over_time)
         .run();
 }
 
@@ -45,6 +49,19 @@ pub struct Star;
 #[derive(Resource, Default)]
 pub struct Score {
     pub value: u32,
+}
+
+#[derive(Resource)]
+pub struct StarSpawnTimer {
+    pub timer: Timer,
+}
+
+impl Default for StarSpawnTimer {
+    fn default() -> Self {
+        StarSpawnTimer {
+            timer: Timer::from_seconds(STAR_SPAWN_TIME, TimerMode::Repeating),
+        }
+    }
 }
 
 pub fn spawn_player(
@@ -271,5 +288,29 @@ pub fn player_hit_star(
 pub fn update_score(score: Res<Score>) {
     if score.is_changed() {
         println!("Score: {}", score.value.to_string());
+    }
+}
+
+pub fn tick_star_spawn_timer(mut star_spawn_timer: ResMut<StarSpawnTimer>, time: Res<Time>) {
+    star_spawn_timer.timer.tick(time.delta());
+}
+
+pub fn spawn_stars_over_time(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+    star_spawn_timer: Res<StarSpawnTimer>,
+) {
+    if star_spawn_timer.timer.is_finished() {
+        let window = window_query.single().unwrap();
+
+        let random_x = random::<f32>() * (window.width() - STAR_SIZE) + STAR_SIZE / 2.0;
+        let random_y = random::<f32>() * (window.height() - STAR_SIZE) + STAR_SIZE / 2.0;
+
+        commands.spawn((
+            Sprite::from_image(asset_server.load("sprites/star.png")),
+            Transform::from_xyz(random_x, random_y, 0.0),
+            Star,
+        ));
     }
 }
