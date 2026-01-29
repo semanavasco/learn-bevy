3cbb99d4b339190dc21190b8a9f8bb1be60abad1
semanavasco <semanavasco@gmail.com>
2026-01-29 22:49:24 +0100

    feat: add enemy spawn over time

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,14 @@ pub const ENEMY_SPEED: f32 = 200.0;
 pub const NUMBER_OF_STARS: usize = 10;
 pub const STAR_SIZE: f32 = 30.0;
 pub const STAR_SPAWN_TIME: f32 = 1.0;
+pub const ENEMY_SPAWN_TIME: f32 = 5.0;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .init_resource::<Score>()
         .init_resource::<StarSpawnTimer>()
+        .init_resource::<EnemySpawnTimer>()
         .add_systems(Startup, spawn_camera)
         .add_systems(Startup, spawn_player)
         .add_systems(Startup, spawn_enemies)
@@ -27,6 +29,8 @@ fn main() {
         .add_systems(Update, update_score)
         .add_systems(Update, tick_star_spawn_timer)
         .add_systems(Update, spawn_stars_over_time)
+        .add_systems(Update, tick_enemy_spawn_timer)
+        .add_systems(Update, spawn_enemies_over_time)
         .run();
 }
 
@@ -60,6 +64,19 @@ impl Default for StarSpawnTimer {
     fn default() -> Self {
         StarSpawnTimer {
             timer: Timer::from_seconds(STAR_SPAWN_TIME, TimerMode::Repeating),
+        }
+    }
+}
+
+#[derive(Resource)]
+pub struct EnemySpawnTimer {
+    pub timer: Timer,
+}
+
+impl Default for EnemySpawnTimer {
+    fn default() -> Self {
+        EnemySpawnTimer {
+            timer: Timer::from_seconds(ENEMY_SPAWN_TIME, TimerMode::Repeating),
         }
     }
 }
@@ -311,6 +328,36 @@ pub fn spawn_stars_over_time(
             Sprite::from_image(asset_server.load("sprites/star.png")),
             Transform::from_xyz(random_x, random_y, 0.0),
             Star,
+        ));
+    }
+}
+
+pub fn tick_enemy_spawn_timer(mut enemy_spawn_timer: ResMut<EnemySpawnTimer>, time: Res<Time>) {
+    enemy_spawn_timer.timer.tick(time.delta());
+}
+
+pub fn spawn_enemies_over_time(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+    enemy_spawn_timer: Res<EnemySpawnTimer>,
+) {
+    if enemy_spawn_timer.timer.is_finished() {
+        let window = window_query.single().unwrap();
+
+        let random_x = random::<f32>() * (window.width() - CHARACTERS_SIZE) + CHARACTERS_SIZE / 2.0;
+        let random_y =
+            random::<f32>() * (window.height() - CHARACTERS_SIZE) + CHARACTERS_SIZE / 2.0;
+
+        commands.spawn((
+            Sprite::from_image(asset_server.load("sprites/ball_red_large.png")),
+            Transform::from_xyz(random_x, random_y, 0.0),
+            Enemy {
+                direction: Vec2::new(random::<f32>(), random::<f32>()).normalize(),
+            },
+            Confined {
+                size: CHARACTERS_SIZE,
+            },
         ));
     }
 }
